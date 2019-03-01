e2c87ea200d2275e8cc328ef466a3155963a2bb8
John Doe <victorKhovTko@yandex.ru>
2019-03-01 15:09:24 +0300

    Added TXT FILE

--- source/FinalTest/Task02/task-02-2.h
+++ source/FinalTest/Task02/task-02-2.h
@@ -5,14 +5,15 @@ struct Node
 {
     std::string Name;
     int loyalty = 0;
+    Node *next = nullptr;
 };
 
-void insertNode(Node **&list, int &listSize, const std::string &secondName, int const loyalty);
+void insertNode(Node *&list, int &listSize, const std::string &secondName, int const loyalty);
 
-void sortByLoyalty(Node **list, int const listSize);
+void sortByLoyalty(Node *list, int const listSize);
 
-void sortBySecondName(Node **list, int const listSize);
+void sortBySecondName(Node *list, int const listSize);
 
-void printList(Node **list, int const listSize);
+void printList(Node *list, int const listSize);
 
 void StalinInterface(int killCount = 0, int sendCount = 0);
