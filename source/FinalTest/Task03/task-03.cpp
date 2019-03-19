b6b2f1bd4966954a27cc1dd0247839a2cb71d157
John Doe <victorKhovTko@yandex.ru>
2019-03-19 17:36:35 +0300

    Added graph implememntation

---
+++ source/FinalTest/Task03/task-03.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vic on 3/19/2019.
+//
+
+#include "task-02.h"
---
+++ source/FinalTest/Task03/task-03.h
@@ -0,0 +1,8 @@
+//
+// Created by vic on 3/19/2019.
+//
+
+#ifndef SPBU_TESTWORKS_TASK_02_H
+#define SPBU_TESTWORKS_TASK_02_H
+
+#endif //SPBU_TESTWORKS_TASK_02_H
