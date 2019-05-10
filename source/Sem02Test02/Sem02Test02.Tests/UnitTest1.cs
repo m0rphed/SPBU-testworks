9fa254405d3cd2a514dc3c3215352453b4bf3f74
Vic Khov <victorKhovTko@yandex.ru>
2019-05-10 10:10:13 +0300

    Added empty project template

---
+++ source/Sem02Test02/Sem02Test02.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿namespace Sem02Test02.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public static class SampleTests
+    {
+        [Test]
+        public static void Test01()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ source/Sem02Test02/Sem02Test02/SimpleClass.cs
@@ -0,0 +1,12 @@
+﻿namespace Sem02Test02
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SimpleClass
+    {
+    }
+}
